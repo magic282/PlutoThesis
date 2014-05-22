30ad89c7ab8a4fc383d0c137135d8b069a825f85
magic282 <magic282sub@gmail.com>
2014-05-22 12:37:57 +0800

    Init

---
+++ clean.bat
@@ -0,0 +1,25 @@
+del *.aux/s
+del *.bak
+del *.log
+
+del *.bbl
+del *.dvi
+del *.blg
+del *.thm
+del *.toc
+del *.toe
+
+del *.lof 
+del *.lot
+del *.out 
+
+del *.fen
+del *.ten
+del *.ps
+
+del *.gz
+del *.gz(busy)
+del *.synctex
+del *.synctex(busy)
+
+del *.loa
